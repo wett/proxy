b974da942494c1a208cc3fdf43128cfd4bfffac3
Terry Go <gqlpub@gmail.com>
2016-12-29 10:51:27 +0800

    add sudo to script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 
 clear
-echo ""
+sudo echo ""
 echo "#############################################################"
 echo "# Install  Shadowsocks client                               #"
-echo "# Intro: https://github.com/tgox/autoproxy.git              #"
-echo "# Author: Terry Go <gqlpub@gmail.com>                       #"
+echo "# Intro: https://github.com/wett/proxy.git                  #"
+echo "# Author: Wett Kok <wettkok@gmail.com>                      #"
 echo "#############################################################"
 echo ""
 
 DIR=${HOME}/.proxy
 
 checkos() {
-  printf "checking Operate System"
+  printf "checking Operate System\n"
   if  cat /etc/issue | grep -q -E -i "ubuntu"; then
     OS=ubuntu
-    printf "detected OS is Ubuntu."
+    printf "detected OS is Ubuntu.\n"
   elif cat /proc/version | grep -q -E -i "ubuntu"; then
     OS=ubuntu
-    printf "detected OS is Ubuntu."
+    printf "detected OS is Ubuntu.\n"
   else
-    echo "Not supported Operatintg System."
+    echo "Not supported Operatintg System.\n"
   fi
 }
 
 install_ubuntu() {
-  apt-get install -y python-pip
+  sudo apt-get install -y python-pip
   pip install shadowsocks
 
   git clone https://github.com/rofl0r/proxychains-ng.git ~/.proxy/proxychains-ng
@@ -48,9 +48,9 @@ nohup sslocal -c $DIR/shadowsocks.json  >$DIR/log/shadowsocks.log 2>&1 &
 # Main Install function
 main() {
     checkos
-    printf "git cloning https://github.com/wettk/proxy.git"
+    printf "git cloning https://github.com/wettk/proxy.git\n"
     git clone https://github.com/wettk/proxy.git $DIR
-    print "git clone compeleted"
+    print "git clone compeleted\n"
     install
     run_config
 }
