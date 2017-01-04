ec4915d8bbf5cbd557e6c2f17e290286665a1edd
caco <Wett Kok>
2017-01-03 23:11:58 -0800

    add my own ban ip address

--- script/ssstart.sh
+++ script/ssstart.sh
@@ -6,7 +6,7 @@ SS_CONFIG_DIR=$DIR/ss-config
 
 printf "shadowsocks is running background\n"
 
-nohup sslocal -c $SS_CONFIG_DIR/sslink.json  >$DIR/log/shadowsocks.log 2>&1 &
+nohup sslocal -c $SS_CONFIG_DIR/ban.json  >$DIR/log/shadowsocks.log 2>&1 &
 
 # waits 5  seconds.
 sleep 5s
